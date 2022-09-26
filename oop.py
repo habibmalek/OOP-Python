7697404e68f2d4baa0264a0ffbf70b6043627578
Habib <habib.m.othman@gmail.com>
2022-09-26 21:05:43 +0300

    serialization/de

--- oop.py
+++ oop.py
@@ -1,3 +1,5 @@
+import json
+
 class Human():
     def __init__(self, id, name):
         self.name = name
@@ -29,14 +31,26 @@ class Project():
         self.id = id
         self.name = name
 
-file = open('readme.txt', 'r')
+
+file = open('data.txt', 'r')
 read = file.readlines()
 for x in read:
     y = x.strip().split(" ")
     g = f"{y[0]}({y[1]},{y[2]})"
-    print(g)
     compiled = compile(g, '', 'exec')
     exec(compiled)
+
+
+obj = Employee( 5 , "Loli")
+
+# Serialization
+serialize_jsonvar = json.dumps(obj, default = vars)
+print("This is a serialization of an object to JSON file >>> ",serialize_jsonvar)
+# De-Serialization
+deserialize_jsonvar = json.loads(serialize_jsonvar)
+print("This is a deserialization of JSON file to Python dictionary >>> ",deserialize_jsonvar)
+
+
 
 
 
